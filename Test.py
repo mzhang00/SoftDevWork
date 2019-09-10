b7e40462bf59e2f6483fc3c41b4d86af8524a218
Michael Zhang <mzhang00@stuy.edu>
2019-09-10 08:38:38 -0400

    Update and test lab code

--- Test.py
+++ Test.py
@@ -1,1 +1,15 @@
-print ("Hello Whirled")
+import random
+
+def randStudent(l):
+    firstRand = random.randint(0,2);
+    if (firstRand == 0):
+        firstRand = 'orpheus'
+    elif (firstRand == 1):
+        firstRand = 'rex'
+    else:
+        firstRand = 'endymion'
+    print (l.get(firstRand)[random.randint(0, len(l.get(firstRand)) - 1)])
+
+KREWES = {'orpheus': ['12','as','ad','sadf'], 'rex': ['132','asds','adfgfdgd','sdsgsfadf'], 'endymion': ['2','a','aasdfadf','sad']}
+
+randStudent(KREWES)
