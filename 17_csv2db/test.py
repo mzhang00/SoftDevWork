f83bf76ac2991c6719d092d66ade198821039c70
Amanda Zheng <azheng00@stuy.edu>
2019-10-07 22:22:36 -0400

    FINALLY DEBUGGED THANKS GOD OF HACKERIN/SQLITE/PYTHON :) XD

--- 17_csv2db/test.py
+++ 17_csv2db/test.py
@@ -12,15 +12,23 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()
 
 command = "CREATE TABLE Students (name TEXT, age INTEGER, id INTEGER);" # test SQL stmt in sqlite3 shell, save as string
-#c.execute(command)    # run SQL statement
-
-with open('students.csv') as csvfile:
-     reader = csv.DictReader(csvfile)
+#c.execute(command)
+with open('students.csv') as student:
+     reader = csv.DictReader(student)
      for row in reader:
-         command = "insert into Students (name, age, id) values ('" + row['name'] + "', " + str(row['age']) + ", " + str(row['id']) + ")"
-         #print(command)
-         c.execute(command)    # run SQL statement
-         #print(row['name'], row['age'], row['id'])
+         command = "insert into Students (name, age, id) values ('" + row['name'] + "', " + str(row['age']) + ", " + str(row['id']) + ");"
+         c.execute(command)
+         print(command)
+ # test SQL stmt in sqlite3 shell, save as string
+command =  "CREATE TABLE Courses (code TEXT, mark INTEGER, id INTEGER);"
+c.execute(command)
+with open('courses.csv') as course:
+     reader = csv.DictReader(course)
+     for row in reader:
+         command = "insert into Courses (code, mark, id) values ('" + row['code'] + "', " + str(row['mark']) + ", " + str(row['id']) + ");"
+         c.execute(command)
+         print(command)
+
 
 #==========================================================
 
