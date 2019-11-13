a5845e663cb7cdebeb8af60a011efa589a644f67
Michael Zhang <mzhang00@stuy.edu>
2019-11-13 01:34:16 -0500

    Finish 24_=rest

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
-import urllib3, json
+import urllib.request, json
+from json import loads
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib3.urlopen(
+    u = urllib.request.urlopen(
     "https://api.nasa.gov/planetary/apod?api_key=NHZYuhe3G4IYQftAt1abs1rltUwNxwVaj8YUCenb"
     )
     response = u.read()
     data = json.loads( response )
-    return render_template("index.html", pic = data['url'])
+    return render_template("index.html", pic = data['url'], desc = data['explanation'])
 
 if  __name__ == '__main__':
     app.debug = True
