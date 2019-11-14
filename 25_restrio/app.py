c84706db214877da5b63c8688da165f052bc3b8e
ray. lee <rlee00@stuy.edu>
2019-11-13 19:25:47 -0500

    added tyler1

--- 25_restrio/app.py
+++ 25_restrio/app.py
@@ -16,6 +16,15 @@ def root():
     randomint = random.randint(0,20)
     return render_template("index.html", pic = data['results'][randomint]['image'], name = data['results'][randomint]['name'])
 
+@app.route("/TylerMFN1")
+def tyler1():
+    res = urlopen(
+        "https://en.wikipedia.org/w/api.php?format=json&action=query&formatversion=2&prop=pageimages%7Cpageterms&titles=Tyler1"
+    ).read()
+
+    data = json.loads(res)
+    return render_template("tyler1.html", tylerData=data["query"]["pages"][0]["terms"]["alias"][0])
+
 if  __name__ == '__main__':
     app.debug = True
     app.run()
