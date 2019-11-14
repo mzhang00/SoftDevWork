c373b7b2b87a732675c039212cd2c0860b2be4f6
ray. lee <rlee00@stuy.edu>
2019-11-14 03:42:18 -0500

    added league/riotgames work

---
+++ 25_restrio_E=MCHammer/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template
+import urllib.request, json
+from json import loads
+from urllib.request import urlopen
+import random
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def root():
+    u = urllib.request.urlopen("https://rickandmortyapi.com/api/character/")
+    response = u.read()
+    data = json.loads(response)
+    randomint = random.randint(0, 20)
+    return render_template(
+        "index.html",
+        pic=data["results"][randomint]["image"],
+        name=data["results"][randomint]["name"],
+    )
+
+
+@app.route("/TylerMFN1")
+def tyler1():
+    res = urlopen(
+        "https://en.wikipedia.org/w/api.php?format=json&action=query&formatversion=2&prop=pageimages%7Cpageterms&titles=Tyler1"
+    ).read()
+
+    data = json.loads(res)
+    return render_template(
+        "tyler1.html", tylerData=data["query"]["pages"][0]["terms"]["alias"][0]
+    )
+
+
+@app.route("/league")
+def league():
+    res = urlopen(
+        "http://static.developer.riotgames.com/docs/lol/gameModes.json"
+    ).read()
+
+    data = json.loads(res)
+    return render_template("league.html", leagueData=data)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
