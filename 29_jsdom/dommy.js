152dad406ef286730b8396f219f09a0865a48cc8
Michael Zhang <mzhang00@stuy.edu>
2019-12-13 08:44:16 -0500

    update js file

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -1,49 +1,43 @@
-var changeHeading = function(e, a) {
+var changeHeading = function(e) {
   var h = document.getElementById("h");
-  if (a == 0){
-    h.innerHTML = e;
-  }else{
-    h.innerHTML = "hello";
-  }
-
-}
-
-var changeHeading2 = function(e) {
-  var h = document.getElementById("h");
-  h.innerHTML = "hello";
-}
+  //console.log(h.innerHTML);
+  h.innerHTML = e;
+};
 
 var removeItem = function(e) {
 
-}
+};
 
 var lis = document.getElementsByTagName("li");
-console.log(lis);
+//console.log(lis);
 
 for (var i = 0; i < lis.length; i++){
-  lis[i].addEventListener('mouseover',changeHeading("<b>hello</b>"));
-  lis[i].addEventListener('mouseout',changeHeading("<b>hello</b>"));
-  lis[i].addEventListener('click',changeHeading("Hello World!", 0));
-  lis[i].addEventListener('click',function (e) {console.log(e);});
+  lis[i].addEventListener('mouseover',function (e) {changeHeading(this.innerHTML);});
+  lis[i].addEventListener('mouseout',function (e) {changeHeading("Hello World!");});
+  //lis[i].addEventListener("click",changeHeading("asd", count));
+  lis[i].addEventListener("click",function (e) {console.log(e); console.log("hello")});
 }
 
 var addItem = function(e){
-  // var list =
-  // var item = document.createElement("li");
-  //  = "WORD";
-  //list.???(item);
-}
+  var list = document.getElementsByTagName("li");
+  var item = document.createElement("li");
+  item.appendChild(document.createTextNode("WORD"));
+  //NEED TO FIX THIS BELOW
+  //list[list.length - 1].appendChild(item);
+  //list.appendChild(item);
+};
 
-//var button = document.getElementByID("b");
-//button.addEeventListener('click', addItem);
+var button = document.getElementById("b");
+//console.log(button);
+button.addEventListener('click', function(e) {addItem("WORD"); console.log(lis);});
 
 var addFib = function(e) {
   console.log(e);
-}
+};
 
 var addFib2 = function(e) {
   console.log(e);
-}
+};
 
 var fb = document.getElementById("fb");
 fb.addEventListener('click', addFib);
