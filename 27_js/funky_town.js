d40c252b128872283b373dc9482b9df5016574e3
Michael Zhang <mzhang00@stuy.edu>
2020-01-29 08:35:55 -0500

    spring cleaning

--- 27_js/funky_town.js
+++
@@ -1,9 +0,0 @@
-// Ivan Galakhov and Michael Zhang (Team AlGoreithm)
-// SoftDev1 pd1
-// K#27 -- Sequential Progression
-// 2019-12-10
-
-fact = (n) => (n == 0 ? 1 : fact(n-1)*n);
-fibonacci = (n) => (n == 0 || n == 1 ? 1 : fibonacci(n-1)+fibonacci(n-2));
-gcd = (a, b) => (b == 0 ? a : gcd(b, a%b));
-randomStudent = (l) => l[Math.floor(Math.random()*l.length)];
--- fall/29_jsdom/dommy.js
+++ fall/29_jsdom/dommy.js
@@ -5,7 +5,7 @@ var changeHeading = function(e) {
 };
 
 var removeItem = function(e) {
-
+  e.remove();
 };
 
 var lis = document.getElementsByTagName("li");
@@ -14,30 +14,69 @@ var lis = document.getElementsByTagName("li");
 for (var i = 0; i < lis.length; i++){
   lis[i].addEventListener('mouseover',function (e) {changeHeading(this.innerHTML);});
   lis[i].addEventListener('mouseout',function (e) {changeHeading("Hello World!");});
-  //lis[i].addEventListener("click",changeHeading("asd", count));
-  lis[i].addEventListener("click",function (e) {console.log(e); console.log("hello")});
+  lis[i].addEventListener("click",function (e) {removeItem(this);});
 }
 
 var addItem = function(e){
-  var list = document.getElementsByTagName("li");
+  var list = document.getElementById("thelist");
   var item = document.createElement("li");
-  item.appendChild(document.createTextNode("WORD"));
-  //NEED TO FIX THIS BELOW
-  //list[list.length - 1].appendChild(item);
-  //list.appendChild(item);
+  item.innerHTML = "WORD";
+  item.addEventListener('mouseover',function (e) {changeHeading(this.innerHTML);});
+  item.addEventListener('mouseout',function (e) {changeHeading("Hello World!");});
+  item.addEventListener("click",function (e) {removeItem(this);});
+  list.appendChild(item);
 };
 
 var button = document.getElementById("b");
 //console.log(button);
-button.addEventListener('click', function(e) {addItem("WORD"); console.log(lis);});
+button.addEventListener('click', function(e) {addItem();});
+
+firstfibnum = 1;
+fibnum = 1;
 
 var addFib = function(e) {
-  console.log(e);
+  //console.log(fibnum);
+  var list = document.getElementById("fiblist");
+  var item = document.createElement("li");
+  if (firstfibnum == 1){
+    item.innerHTML = fib(fibnum);
+    list.appendChild(item);  
+    firstfibnum++;
+  }else{
+    item.innerHTML = fib(fibnum);
+    list.appendChild(item);
+    fibnum++;
+  }
 };
 
-var addFib2 = function(e) {
-  console.log(e);
+var fib = function(e) {
+  if (e < 2) {
+    return 1;
+  } else {
+    return fib(e-1) + fib(e-2);
+  }
 };
 
 var fb = document.getElementById("fb");
-fb.addEventListener('click', addFib);
+fb.addEventListener('click', function (e) {addFib();});
+
+factnum = 1;
+
+var addFact = function(e) {
+  var list = document.getElementById("factlist");
+  var item = document.createElement("li");
+  item.innerHTML = fact(factnum);
+  list.appendChild(item);
+  factnum++;
+};
+
+var fact = function(e) {
+  if (e < 2) {
+    return 1;
+  } else {
+    return fact(e-1) * e;
+  }
+};
+
+var fctb = document.getElementById("fctb");
+fctb.addEventListener('click', function (e) {addFact();});
