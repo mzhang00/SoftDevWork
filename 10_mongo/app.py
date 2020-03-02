e0737295106d448b9818f2171fef581496442a31
Michael Zhang <mzhang00@stuy.edu>
2020-03-02 08:40:53 -0500

    Add base pymongo code; add database json file

--- 10_mongo/app.py
+++ 10_mongo/app.py
@@ -1,1 +1,28 @@
-#teamRAM
+#teamRAM - Michael Zhang and Amanda Chen
+#SoftDev pd1
+#K10: Import/Export Bank
+#2020-03-02
+
+from pymongo import MongoClient
+from bson.json_util import loads
+client = MongoClient('localhost')
+db = client.database
+col = db.restaurants
+'''with open('primer-dataset.json','r') as c:
+    for doc in c:
+        col.insert_one(loads(doc[:-1]))'''
+
+def filter_borough(bor):
+    return list(col.find({'borough':bor}))
+
+def filter_zip(zip):
+    return list(col.find({'address.zipcode':zip}))
+
+def filter_zip_grade(zip,grade):
+    return list(r for r in col.find({'address.zipcode':zip,'grades.0.grade':grade}))
+
+def filter_zip_score(zip,score):
+    return list(r for r in col.find({'address.zipcode':zip,'grades.0.score':{'$lt':score}}))
+
+def filter_radius(lat,long,r):
+    return list(col.find({'address.coord':{'$geoWithin':{'$centerSphere':[[long,lat],r/6371000]}}}))
